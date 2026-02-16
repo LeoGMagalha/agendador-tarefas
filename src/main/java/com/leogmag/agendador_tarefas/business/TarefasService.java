4e9fb9536443a5c30d5fefae3d1f19a9f8d5acc0
LeoGMagalha <leosmagalha@gmail.com>
2026-02-16 20:13:37 -0300

    adicionando post tarefas

---
+++ src/main/java/com/leogmag/agendador_tarefas/business/TarefasService.java
@@ -0,0 +1,32 @@
+package com.leogmag.agendador_tarefas.business;
+
+import com.leogmag.agendador_tarefas.business.dto.TarefasDTO;
+import com.leogmag.agendador_tarefas.business.mapper.TarefasConverter;
+import com.leogmag.agendador_tarefas.infrastructure.entity.TarefasEntity;
+import com.leogmag.agendador_tarefas.infrastructure.enums.StatusNotificacao;
+import com.leogmag.agendador_tarefas.infrastructure.repository.TarefasRepository;
+import com.leogmag.agendador_tarefas.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TarefasService {
+
+    private final TarefasRepository tarefasRepository;
+    private final TarefasConverter tarefasConverter;
+    private final JwtUtil jwtUtil;
+
+    public TarefasDTO gravarTarefa(String token, TarefasDTO dto){
+        String email = jwtUtil.extractEmailToken(token.substring(7));
+        dto.setDataCriacao(LocalDateTime.now());
+        dto.setStatusNotificacao(StatusNotificacao.PENDENTE);
+        dto.setEmailUsuario(email);
+        TarefasEntity entity = tarefasConverter.paraTarefasEntity(dto);
+        return tarefasConverter.paraTarefasDTO(
+                tarefasRepository.save(entity)
+        );
+    }
+}
