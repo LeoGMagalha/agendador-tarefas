340229c141e3e7886ededaf11cd68062a40143e9
LeoGMagalha <leosmagalha@gmail.com>
2026-02-16 20:43:09 -0300

    add metodos get

--- src/main/java/com/leogmag/agendador_tarefas/business/TarefasService.java
+++ src/main/java/com/leogmag/agendador_tarefas/business/TarefasService.java
@@ -7,9 +7,11 @@ import com.leogmag.agendador_tarefas.infrastructure.enums.StatusNotificacao;
 import com.leogmag.agendador_tarefas.infrastructure.repository.TarefasRepository;
 import com.leogmag.agendador_tarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +30,14 @@ public class TarefasService {
         return tarefasConverter.paraTarefasDTO(
                 tarefasRepository.save(entity)
         );
+    }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal){
+        return tarefasConverter.paraListaTarefasDTO(tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
+    }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorEmail(String token){
+        String email = jwtUtil.extractEmailToken(token.substring(7));
+        return tarefasConverter.paraListaTarefasDTO(tarefasRepository.findByEmailUsuario(email));
     }
 }
--- src/main/java/com/leogmag/agendador_tarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/leogmag/agendador_tarefas/business/mapper/TarefasConverter.java
@@ -4,8 +4,13 @@ import com.leogmag.agendador_tarefas.business.dto.TarefasDTO;
 import com.leogmag.agendador_tarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
     TarefasEntity paraTarefasEntity(TarefasDTO dto);
     TarefasDTO paraTarefasDTO(TarefasEntity entity);
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> etitys);
+
 }
